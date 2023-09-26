96013287d60fa0920e852281c3d8f30c1780ea41
Sergey <pravotorov.sergey@gmail.com>
2023-09-26 23:10:41 +0300

    2609NOCH

--- Vision.py
+++ Vision.py
@@ -2,19 +2,51 @@ import cv2
 import numpy as np
 import socket
 import random
-#from keras import models
-#import tensorflow as tf
+from keras import models
+import tensorflow as tf
 
 
 if __name__ == '__main__':
    def callback(*arg):
       print (arg)
 
+buffer = []
+buffer_test_in = []
+buffer_test_out = []
+data = 'photo'
+marker = 0
+i = 0
+n = 0
+
+bad = 0 
+find = 0
+
+robot_xmin = 318
+robot_xmax = 128
+robot_ymin = -391
+robot_ymax = -145
+
+
 def pass_func(x):
    pass
 
-def ranomizer(x, y, r, d):
+def randomizer(x, y, r, d):
+    global i
+    global bad
+    global t
 
+    global myseq
+    global defect
+    global deltax 
+    global deltay 
+    global size 
+    global color 
+    global line1 
+    global line2 
+    global rand1
+    global rand2
+    
+    
     list_random = [1] + [0] * d
     t = random.choice(list_random)
 
@@ -35,22 +67,23 @@ def ranomizer(x, y, r, d):
             rand2 = 0
 
         if rand1 == 1:
+            cv2.putText(output_screen, str(defect), (x-deltax, y - deltay), cv2.FONT_ITALIC, size, (color, color, color), line1, cv2.LINE_AA)
             cv2.putText(output_ai, str(defect), (x-deltax, y - deltay), cv2.FONT_ITALIC, size, (color, color, color), line1, cv2.LINE_AA)
         
         if rand2 == 1:
+            cv2.circle(output_screen, (x, y), r, (color, color, color), line2)
             cv2.circle(output_ai, (x, y), r, (color, color, color), line2)
+    
+        cv2.circle(output_screen, (x, y), r + 5, (255, 0, 0), 2)
 
+        bad += 1
+    else: 
+        cv2.circle(output_screen, (x, y), r + 5, (0, 255, 0), 2)
 
-buffer = []
-data = 'photo'
-marker = 0
-i = 0
-n = 0
+    i += 1
+    cv2.putText(output_screen, str(i), (x-5, y - 10), cv2.FONT_ITALIC, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+    cv2.rectangle(output_screen, (x - 2, y - 2), (x + 2, y + 2), (0, 128, 255), -1)
 
-robot_xmin = 318
-robot_xmax = 128
-robot_ymin = -391
-robot_ymax = -145
 
 cv2.namedWindow('frame')
 cv2.namedWindow('setting')
@@ -74,7 +107,7 @@ cv2.createTrackbar('minRadius', 'setting2', 18, 100, pass_func)
 cv2.createTrackbar('maxRadius', 'setting2', 28, 200, pass_func)
 cv2.createTrackbar('photo', 'setting2', 0, 1, pass_func)
 
-#model = tf.keras.saving.load_model("myaphly_model_local.keras")
+model = tf.keras.saving.load_model("myaphly_model_local.keras")
 
 """s = socket.socket()
 s.bind(('192.168.1.10', 502))
@@ -109,6 +142,8 @@ while True:
         marker = 0
         cv2.imwrite(f'photo/main.png', frame)
         cv2.setTrackbarPos('photo', 'setting2', 1)
+        bad = 0
+        find =0
         #data = 0
 
     frame = cv2.imread('photo/main.png')
@@ -132,46 +167,45 @@ while True:
         circles = np.round(circles[0, :]).astype("int")
 
         for (x, y, r) in circles:
-            
-            ranomizer(x, y, r, 2)
-
-            i += 1
-            cv2.putText(output_screen, str(i), (x-5, y - 10), cv2.FONT_ITALIC, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
-            cv2.circle(output_screen, (x, y), r, (0, 255, 0), 3)
-            cv2.rectangle(output_screen, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
-            
-            wcropp, hcropp = 48, 48
-            cropp = output_ai[int(y - hcropp/2): int(y + hcropp/2), int(x - wcropp/2): int(x + wcropp/2)]
-            
-            cropp_gray = cv2.cvtColor(cropp, cv2.COLOR_BGR2GRAY)
-
+           
             if  marker == 0:
-                n += 1
-                cv2.imwrite(f'Data_bad/{n}.png', cropp_gray)
-            
-            cropp = np.expand_dims(cropp, axis = 0)        
+                randomizer(x, y, r, 4)
+                
+                wcropp, hcropp = 48, 48
+                cropp = output_ai[int(y - hcropp/2): int(y + hcropp/2), int(x - wcropp/2): int(x + wcropp/2)]
+                
+                cropp_gray = cv2.cvtColor(cropp, cv2.COLOR_BGR2GRAY)
+                #n += 1
+                #cv2.imwrite(f'Data_bad/{n}.png', cropp_gray)
             
             dx_transp = robot_xmin - int((robot_xmax - robot_xmin)/weight_frame*x)
             dy_transp = robot_ymin + int((robot_ymax - robot_ymin)/height_frame*y)
 
-            #predict = model.predict(cropp)
-            #print(predict)
-            #predict = predict.reshape(2)
+            cropp = np.expand_dims(cropp_gray, axis = 0)         
 
-            predict = 1
-            
-            if predict >= 0.7:           
+            predict = model.predict(cropp)
+            print(predict)
+            predict = predict.reshape(2)
+         
+            if predict[0] >= 0.7 and t == 1:         
                 buffer.insert(i - 1, [str.encode(f'{dx_transp}') ,str.encode(f'{dy_transp}')])
-        
+                
+                find += 1
+                cv2.circle(output_screen, (x, y), r, (0, 0, 255), 5)
+            
+            if predict[0] >= 0.7 and t != 1:    
+                print('Error {i}')
+
+            if predict[1] >= 0.7 and t == 1:    
+                print('Error {i}')
+
         i = 0
 
         if  marker == 0:
-            print('НЕОТСОРТИРОВАННЫЙ:') 
-            print(buffer) 
 
             buffer.sort(key = lambda x: x[0], reverse = True)
-            print('ОТСОРТИРОВАННЫЙ:') 
-            print(buffer) 
+            #print('ОТСОРТИРОВАННЫЙ:') 
+            #print(buffer) 
             print('Передача:') 
                         
             if len(buffer) > 0:
@@ -183,7 +217,14 @@ while True:
                 print(str.encode(f'{len(buffer)}') + b'\r\n') 
             
             marker = 1
-                
+
+            cv2.imwrite(f'photo/screen.png', output_screen)
+
+    output_screen = cv2.imread('photo/screen.png')
+
+    cv2.putText(output_screen, 'FIND  ' + str(find), (40, 50), cv2.FONT_ITALIC, 1.5, (255, 255, 255), 1, cv2.LINE_AA)               
+    cv2.putText(output_screen, 'BAD  '+ str(bad), (40, 100), cv2.FONT_ITALIC, 1.5, (255, 255, 255), 1, cv2.LINE_AA)   
+    
     cv2.imshow("frame", output_screen)
     cv2.imshow("gray", gray)
 
